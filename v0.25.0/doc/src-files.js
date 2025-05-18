9adf7929926dd96e51e2474dc11a3d3c59b4e260
davidhewitt <davidhewitt@users.noreply.github.com>
2025-05-18 14:08:59 +0000

    Upload documentation for v0.25.0

---
+++ v0.25.0/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["pyo3_async_runtimes",["",[],["async_std.rs","err.rs","generic.rs","lib.rs","testing.rs","tokio.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[103]}
---
+++ v0.25.0/doc/trait.impl/pyo3_async_runtimes/generic/trait.JoinError.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["pyo3_async_runtimes",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[26]}
