b84d15fd54de40c9b74ff7c57e7771909959e8c1
davidhewitt <davidhewitt@users.noreply.github.com>
2025-09-02 19:25:08 +0000

    Upload documentation for v0.26.0

---
+++ v0.26.0/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["pyo3_async_runtimes"];
+//{"start":21,"fragment_lengths":[21]}
---
+++ v0.26.0/doc/trait.impl/inventory/trait.Collect.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["pyo3_async_runtimes",[["impl <a class=\"trait\" href=\"https://docs.rs/inventory/0.3.21/inventory/trait.Collect.html\" title=\"trait inventory::Collect\">Collect</a> for <a class=\"struct\" href=\"pyo3_async_runtimes/testing/struct.Test.html\" title=\"struct pyo3_async_runtimes::testing::Test\">Test</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[310]}
