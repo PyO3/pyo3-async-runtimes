7a1f44bdaf39850e29fe0bdbef896692bc542684
davidhewitt <davidhewitt@users.noreply.github.com>
2026-02-04 11:45:36 +0000

    Upload documentation for v0.28.0

---
+++ v0.28.0/doc/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["pyo3_async_runtimes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pyo3_async_runtimes/testing/struct.Args.html\" title=\"struct pyo3_async_runtimes::testing::Args\">Args</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[317]}
---
+++ v0.28.0/doc/trait.impl/pyo3/type_object/trait.PyTypeInfo.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["pyo3_async_runtimes",[["impl PyTypeInfo for <a class=\"struct\" href=\"pyo3_async_runtimes/err/struct.RustPanic.html\" title=\"struct pyo3_async_runtimes::err::RustPanic\">RustPanic</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[191]}
